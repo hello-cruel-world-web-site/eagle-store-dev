c21420cbee8b411a6e87d08c9fb1b33443569aeb
GustavoLeao2018 <gustavo.leao.nogueira.2017@gmail.com>
2019-05-09 15:10:14 -0300

    Base de código do site criada

---
+++ src/js/escript.js
@@ -0,0 +1,9 @@
+/*
+ *    Eagle Script
+ */
+$("document").ready(function() {
+    $("#sticky").on("click", function() {
+        $("nav").toggleClass("nav-sticky");
+        console.log('clicado!!!');
+    });
+});
